0b8d3469a962e9c6095c32e2af0924d0d93c2b5d
Aisamuddin <aisam0094@gmail.com>
2023-01-30 22:36:30 +0800

    improve authentication, add select drinks, and fix total cash held

--- src/components/MaintainerControlPanel.jsx
+++ src/components/MaintainerControlPanel.jsx
@@ -11,56 +11,76 @@ function MaintainerControlPanel() {
   const [totalDenomination, setTotalDenomination] = useState(0);
   const [newDrinkPrice, setNewDrinkPrice] = useState();
   const [totalCashHeld, setTotalCashHeld] = useState(0);
+  const [drinks, setDrinks] = useState([]);
   const [coins, setCoins] = useState();
   const [coinSelected, setCoinSelected] = useState();
+  const [drinkSelected, setDrinkSelected] = useState("");
 
   async function getCoinData() {
     const coinData = await axios.get(`http://localhost:8000/api/coins`);
     setCoins(coinData?.data?.coins);
   }
 
-  function getTotalCashHeld() {
+  async function fetchDrinks() {
+    const { data } = await axios.get(`http://127.0.0.1:8000/api/drinks`);
+    console.log(data);
+    setDrinks(data?.drinks);
+  }
 
-    let totalCashHeld = 0;
-    axios.get(`http://localhost:8000/api/coins`).then((res) => {
-      console.log(res.data.coins);
-      for (let i = 0; i < coins?.length; i++) {
-        // totalCashHeld += coinValue[data[i].denomination] * data[i].quantity;
-        totalCashHeld += coins[i].count * coins[i].value;
-      }
-      setTotalCashHeld(totalCashHeld);
-    });
+  async function getTotalCashHeld() {
+    let totalCash = 0;
+    const { data } = await axios.get(`http://localhost:8000/api/coins`);
+    let coins = data.coins;
+    for (let i = 0; i < coins?.length; i++) {
+      totalCash += coins[i].count * coins[i].value;
+    }
+    setTotalCashHeld(totalCash);
   }
 
   function selectCoin(id) {
     for (let i = 0; i < coins?.length; i++) {
-      if (id === coins[i]['id']) {
+      if (id === coins[i]["id"]) {
         setCoinSelected(coins[i]);
       }
     }
   }
 
-  function verifyPassword(password) {
-    if (password.length == 6) {
-      axios.post(`http://localhost:8000/api/verify-password`, {
-        "password": password
-      }).then((res) => {
-        if (res.status === 200) {
-          setIsPasswordValid(true);
-        }
-      }).catch(e => {
-        console.log("invalid password");
-        setIsPasswordValid(false);
-      });
+  function selectDrink(id) {
+    for (let i = 0; i < drinks?.length; i++) {
+      if (id === drinks[i]["id"]) {
+        setDrinkSelected(drinks[i]);
+      }
     }
   }
 
-  // function updateDrinks(target) {
-  // }
+  function verifyPassword(password) {
+    if (password.length === 6) {
+      axios
+        .post(`http://localhost:8000/api/verify-password`, {
+          password: password,
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            setIsPasswordValid(true);
+          }
+        })
+        .catch((e) => {
+          console.log("invalid password");
+          setIsPasswordValid(false);
+        });
+    } else {
+      setIsPasswordValid(false);
+    }
+  }
+
+  function updateDrinks(drinkId, newPrice) {
+
+  }
 
   useEffect(() => {
-    getTotalCashHeld();
     getCoinData();
+    getTotalCashHeld();
+    fetchDrinks();
   }, []);
 
   return (
@@ -72,86 +92,137 @@ function MaintainerControlPanel() {
         <span className="p-2 capitalize bg-primary w-1/2 text-center">
           type password here
         </span>
-        <input type="text" className="bg-secondary w-1/2" maxLength={6} onKeyUp={(e) => verifyPassword(e.target.value)}></input>
+        <input
+          type="text"
+          className="bg-secondary w-1/2"
+          maxLength={6}
+          onKeyUp={(e) => verifyPassword(e.target.value)}
+        ></input>
       </div>
 
       {/* Password validity */}
       <div className="flex mx-auto w-full justify-evenly my-1">
-        <span className={`py-2 px-6 capitalize text-center ${!isPasswordValid ? 'bg-secondary' :'bg-primary'}`}>
+        <span
+          className={`py-2 px-6 capitalize text-center ${
+            !isPasswordValid ? "bg-secondary" : "button-on"
+          }`}
+        >
           password valid
         </span>
-        <span className={`py-2 px-6 capitalize text-center ${isPasswordValid ? 'bg-secondary' :'bg-primary'}`}>
+        <span
+          className={`py-2 px-6 capitalize text-center ${
+            isPasswordValid ? "bg-secondary" : "button-on"
+          }`}
+        >
           password invalid
         </span>
       </div>
 
-      {/* Coin denomination */}
-      <div className="flex flex-col items-center my-3 w-full">
-        <div className="capitalize bg-primary p-2 w-full text-center">
-          press below to determine number of coins in selected denomination
-        </div>
-        <div className="flex py-2 w-full text-center bg-secondary justify-evenly mt-1">
-          {coins?.map((value, index) => {
-            return <button key={index} onClick={() => selectCoin(value.id)}>{value.type}</button>
-          })}
-        </div>
-        <div className="panelbar">
-          <div className="capitalize bg-primary w-1/2 p-1 text-center">
-            total number of coins in selected denomination
+      {isPasswordValid && (
+        <div>
+          {/* Coin denomination */}
+          <div className="flex flex-col items-center my-3 w-full">
+            <div className="capitalize bg-primary p-2 w-full text-center">
+              press below to determine number of coins in selected denomination
+            </div>
+            <div className="flex py-2 w-full text-center bg-secondary justify-evenly mt-1">
+              {coins?.map((value, index) => {
+                return (
+                  <button key={index} onClick={() => selectCoin(value.id)}>
+                    {value.type}
+                  </button>
+                );
+              })}
+            </div>
+            <div className="panelbar">
+              <div className="capitalize bg-primary w-1/2 p-1 text-center">
+                total number of coins in selected denomination
+              </div>
+              <div className="flex bg-secondary w-1/2 justify-center items-center">
+                {coinSelected?.count}
+              </div>
+            </div>
           </div>
-          <div className="flex bg-secondary w-1/2 justify-center items-center">
-            {coinSelected?.count}
+
+          {/* New drinks price */}
+          <div className="grid py-2 grid-cols-3 w-full text-center bg-secondary justify-evenly mt-1">
+            {drinks?.map((drink, index) => {
+              if (drink.brand === drinkSelected.brand) {
+                return (
+                  <button
+                    className="button-on"
+                    key={index}
+                    onClick={() => selectDrink(drink.id)}
+                  >
+                    {drink.brand}
+                  </button>
+                );
+              } else {
+                return (
+                  <button
+                    className=""
+                    key={index}
+                    onClick={() => selectDrink(drink.id)}
+                  >
+                    {drink.brand}
+                  </button>
+                );
+              }
+            })}
+          </div>
+          <div className="panelbar">
+            <div className="capitalize bg-primary p-2 text-center w-1/2">
+              type new drinks can price here
+            </div>
+            <input
+              className="bg-secondary w-1/2"
+              type="text"
+              onKeyUp={(e) => updateDrinks(e.target.value)}
+            ></input>
+          </div>
+
+          {/* Total cash held by machine */}
+          <div className="panelbar">
+            <div className="bg-primary capitalize w-1/2 text-center p-1">
+              {/* press here for total cash held by machine */}
+              total cash held by machine
+            </div>
+            <div className="bg-secondary w-1/2 flex justify-center items-center p-1">
+              RM {totalCashHeld.toFixed(2)}
+            </div>
+          </div>
+
+          {/* Cash collection */}
+          <div className="panelbar bg-primary">
+            <div className="bg-primary w-1/2 capitalize flex justify-center items-center">
+              press here to collect all cash
+            </div>
+            <div className="w-1/2 flex justify-center items-center">
+              <button className="">Press</button>
+            </div>
+          </div>
+
+          {/* Cash dispenser */}
+          <div className="panelbar bg-primary py-1">
+            <div className="bg-primary w-1/2 capitalize flex justify-center items-center">
+              collect all cash here
+            </div>
+            <div className="w-1/2 flex justify-center items-center">
+              <div className="io-interface"></div>
+            </div>
+          </div>
+
+          {/* Finish operation */}
+          <div className="panelbar bg-primary py-1">
+            <div className="bg-primary w-1/2 capitalize flex justify-center items-center">
+              press here when finished
+            </div>
+            <div className="w-1/2 flex justify-center items-center">
+              <button className="">Press</button>
+            </div>
           </div>
         </div>
-      </div>
-
-      {/* New drinks price */}
-      <div className="panelbar">
-        <div className="capitalize bg-primary p-2 text-center w-1/2">
-          type new drinks can price here
-        </div>
-        <input className="bg-secondary w-1/2" type="text" onKeyUp={(e) => updateDrinks(e.target.value)}></input>
-      </div>
-
-      {/* Total cash held by machine */}
-      <div className="panelbar">
-        <div className="bg-primary capitalize w-1/2 text-center p-1">
-          press here for total cash held by machine
-        </div>
-        <div className="bg-secondary w-1/2 flex justify-center items-center p-1">
-          RM {totalCashHeld.toFixed(2)}
-        </div>
-      </div>
-
-      {/* Cash collection */}
-      <div className="panelbar bg-primary">
-        <div className="bg-primary w-1/2 capitalize flex justify-center items-center">
-          press here to collect all cash
-        </div>
-        <div className="w-1/2 flex justify-center items-center">
-          <button className="">Press</button>
-        </div>
-      </div>
-
-      {/* Cash dispenser */}
-      <div className="panelbar bg-primary py-1">
-        <div className="bg-primary w-1/2 capitalize flex justify-center items-center">
-          collect all cash here
-        </div>
-        <div className="w-1/2 flex justify-center items-center">
-          <div className="io-interface"></div>
-        </div>
-      </div>
-
-      {/* Finish operation */}
-      <div className="panelbar bg-primary py-1">
-        <div className="bg-primary w-1/2 capitalize flex justify-center items-center">
-          press here when finished
-        </div>
-        <div className="w-1/2 flex justify-center items-center">
-          <button className="">Press</button>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
